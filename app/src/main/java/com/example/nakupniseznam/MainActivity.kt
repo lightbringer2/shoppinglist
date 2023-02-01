3185aaf87a4100435f78e4390f69ee309954cfbe
lightbringer <jnbalcarek@gmail.com>
2023-02-01 15:27:42 +0100

    Update activities

--- app/src/main/java/com/example/nakupniseznam/MainActivity.kt
+++ app/src/main/java/com/example/nakupniseznam/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.nakupniseznam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnWrite:Button
+    lateinit var btnRead:Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRead=findViewById(R.id.btnRead)
+        btnWrite=findViewById(R.id.btnWrite)
+
+        btnRead.setOnClickListener{
+            val intent= Intent(this@MainActivity, Write::class.java)
+            startActivity(intent)
+        }
+
+        btnWrite.setOnClickListener{
+            val intent=Intent(this@MainActivity, Read::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/nakupniseznam/SplashScreen.kt
+++ app/src/main/java/com/example/nakupniseznam/SplashScreen.kt
@@ -13,10 +13,11 @@ class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
+        window.insetsController!!.hide(android.R.style.Theme_Black_NoTitleBar_Fullscreen)
+        /*window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        )*/
         setContentView(R.layout.activity_splash_screen)
 
         Handler(Looper.myLooper()!!).postDelayed({
