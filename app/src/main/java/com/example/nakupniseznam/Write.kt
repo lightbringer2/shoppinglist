f05e31391bee9eb1c59023d680b8130792efe6dd
lightbringer <jnbalcarek@gmail.com>
2023-02-01 17:21:47 +0100

    write update
    přidáno zápis do gsheet

--- app/src/main/java/com/example/nakupniseznam/Write.kt
+++ app/src/main/java/com/example/nakupniseznam/Write.kt
@@ -2,10 +2,65 @@ package com.example.nakupniseznam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class Write : AppCompatActivity() {
+
+    lateinit var wProcessLayout:RelativeLayout
+    lateinit var wProgressBar:ProgressBar
+    lateinit var edName:EditText
+    lateinit var edNum:EditText
+    lateinit var edLoc:EditText
+    lateinit var edPrice:EditText
+    lateinit var btnSave:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+
+        wProcessLayout=findViewById(R.id.wProcessLayout)
+        wProgressBar=findViewById(R.id.wProgressBar)
+        edName=findViewById(R.id.edName)
+        edNum=findViewById(R.id.edNum)
+        edLoc=findViewById(R.id.edLoc)
+        edPrice=findViewById(R.id.edPrice)
+        btnSave=findViewById(R.id.btnSave)
+
+        wProcessLayout.visibility=View.GONE
+        wProgressBar.visibility=View.GONE
+
+
+        btnSave.setOnClickListener {
+            if (edName.text.toString().isEmpty()){
+                //zobraz doplň data
+            }
+            else {
+                val url = ""
+                val stringRequest = object : StringRequest(Request.Method.POST, url,
+                    Response.Listener {
+                        Toast.makeText(this@Write, it.toString(), Toast.LENGTH_SHORT).show()
+                    },
+                    Response.ErrorListener {
+                        Toast.makeText(this@Write, it.toString(), Toast.LENGTH_SHORT).show()
+                    }){
+                    override fun getParams(): MutableMap<String, String>? {
+                        val params=HashMap<String, String>()
+                        params["Název"]=edName.text.toString()
+                        params["Počet"]=edNum.text.toString()
+                        params["Umístění"]=edLoc.text.toString()
+                        params["Cena"]=edPrice.text.toString()
+
+                        return params
+                    }
+            }
+                val queue= Volley.newRequestQueue(this@Write)
+                queue.add(stringRequest)
+            }
+        }
     }
 }
