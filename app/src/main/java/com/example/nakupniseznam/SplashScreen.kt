7d8d7d5136387a1f6281a9b83864e86d4cceec5e
lightbringer <jnbalcarek@gmail.com>
2023-02-01 09:56:07 +0100

    Init
    Init po úpravách.

---
+++ app/src/main/java/com/example/nakupniseznam/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.example.nakupniseznam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3500)
+
+    }
+}
